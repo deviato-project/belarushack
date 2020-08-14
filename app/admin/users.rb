f776ef32c42073ef6360814db929f0d31d3e63d4
Ivan <vtakarlikov@gmail.com>
2020-08-15 02:58:49 +0800

    Improve admin page, add index and show of users

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,18 +1,81 @@
 ActiveAdmin.register User do
+  permit_params :user_type_id, :name, :contact, :surname, :title, :description, :funding_reason, :funding_amount_cents, :funding_amount_currency, :status
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :user_type_id, :name, :contact, :surname, :title, :description, :funding_reason, :funding_amount_cents, :funding_amount_currency, :status
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:user_type_id, :name, :contact, :surname, :title, :description, :funding_reason, :funding_amount_cents, :funding_amount_currency, :status]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+   member_action :take_it, method: :put do
+    resource.update(responsible_admin_user: current_admin_user)
+    redirect_to resources_path, notice: 'Взят в работу!'
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :user_type
+    column :status do |user|
+      User::STATUSES_RU[user.status.to_sym]
+    end
+    column :email
+    column :contact
+    column :name
+    column :surname
+    column :title
+    column :description do |user|
+      user.description.truncate(50, omission: '...') if user.description.present?
+    end
+    column :funding_reason
+    column :funding_amount
+    column :attachments do |user|
+      user.attachments.count
+    end
+    column :responsible_admin_user do |user|
+      if user.responsible_admin_user.blank?
+        link_to 'Взять себе!', take_it_admin_user_path(user), method: :put
+      else
+        link_to(
+          user.responsible_admin_user.name || user.responsible_admin_user.email,
+          admin_admin_user_path(user.responsible_admin_user)
+        )
+      end
+    end
+    column :created_at
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :user_type
+      row :status do |user|
+        User::STATUSES_RU[user.status.to_sym]
+      end
+      row :email
+      row :contact
+      row :name
+      row :surname
+      row :title
+      row :description do |user|
+        user.description.truncate(50, omission: '...') if user.description.present?
+      end
+      row :funding_reason
+      row :funding_amount
+      row :attachments do |user|
+        user.attachments.map do |attachment|
+          [
+            image_tag(url_for(attachment), height: '300'),
+            link_to('Скачать', rails_blob_path(attachment, disposition: "attachment"))
+          ]
+        end
+      end
+      row :responsible_admin_user do |user|
+        if user.responsible_admin_user.blank?
+          link_to 'Взять себе!', take_it_admin_user_path(user), method: :put
+        else
+          link_to(
+            user.responsible_admin_user.name || user.responsible_admin_user.email,
+            admin_admin_user_path(user.responsible_admin_user)
+          )
+        end
+      end
+      row :created_at
+      row :updated_at
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,23 @@
 class User < ApplicationRecord
+  ALLOWED_ATTACHMENT_TYPES = %w(application/pdf image/jpeg image/png)
+  STATUSES_RU = {
+    pending: 'В ожидании',
+    processing: 'В обработке',
+    approved: 'Выплата одобрена',
+    payout_sent: 'Выплата произведена',
+    rejected: 'Отказано'
+  }.freeze
+  enum status: { pending: 0, processing: 1, approved: 2, payout_sent: 3, rejected: 4 }
   monetize :funding_amount_cents
   has_many_attached :attachments
   belongs_to :user_type
+  belongs_to :responsible_admin_user, class_name: 'AdminUser', optional: true
+
+  validate :correct_attachments_mime_type
+
+  def correct_attachments_mime_type
+    if attachments.attached? && !attachments.all? { |attachment| attachment.content_type.in?(ALLOWED_ATTACHMENT_TYPES) }
+      errors.add(:attachments, 'Must be a PDF or a png/jpg/jpeg file')
+    end
+  end
 end
