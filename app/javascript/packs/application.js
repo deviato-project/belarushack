98a1a4bb0755d3974653f5853fd97a1376dfe6ac
Ivan Takarlikov <vtakarlikov@gmail.com>
2020-08-15 03:02:24 +0800

    Base user form page (#3)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,18 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import 'materialize-css/dist/js/materialize';
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('select');
+  var options = document.querySelectorAll('options');
+  var instances = M.FormSelect.init(elems, options);
+});
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
