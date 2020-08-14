35f9427f408a26397052fdcdcab3009bec72dbd6
Ivan <vtakarlikov@gmail.com>
2020-08-15 03:17:49 +0800

    initial deployment script

---
+++ config/deploy.rb
@@ -0,0 +1,85 @@
+# Change these
+# server 'course.beardcode.ru', roles: [:web, :app, :db], primary: true
+
+set :repo_url,        'git@github.com:deviato-project/belarushack.git'
+set :application,     'belarushack'
+set :user,            'deploy'
+set :puma_threads,    [1, 4]
+set :puma_workers,    0
+
+# Don't change these unless you know what you're doing
+set :pty,             true
+set :use_sudo,        false
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to true if using ActiveRecord
+
+set :init_system, :systemd
+
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.6.1'
+## Defaults:
+# set :scm,           :git
+set(:branch)       { ENV['BRANCH'] || 'master' }
+# set :format,        :pretty
+set :log_level,     :debug
+set :keep_releases, 5
+
+## Linked Files & Directories (Default None):
+set :linked_files, %w{config/master.key config/database.yml}
+set :linked_dirs, %w(tmp/pids tmp/sockets log)
+# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
+
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      # unless `git rev-parse HEAD` == "git rev-parse origin/#{fetch(:branch)}"
+      #   puts "WARNING: HEAD is not the same as origin/#{fetch(:branch)}"
+      #   puts "Run `git push` to sync changes."
+      #   exit
+      # end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:stop'
+      invoke 'puma:start'
+    end
+  end
+
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,4 @@
+server 'app.deviato.co', user: 'deploy', roles: [:web, :app, :db], primary: true
+set :stage, :production
+
+set :rails_env, 'production'
